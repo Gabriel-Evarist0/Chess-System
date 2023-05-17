ef9c4f86b35c56d946af0264c3f752be9ebdb1ed
Gabriel-Evarist0 <machado.gabrielevaristo@gmail.com>
2023-05-17 10:51:14 -0300

    Rei e Torre criados

--- src/Chess/ChessMatch.java
+++ src/Chess/ChessMatch.java
@@ -1,12 +1,15 @@
 package Chess;
 
 import BoardGame.Board;
+import BoardGame.Position;
+import Chess.ChessPieces.Rook;
 
 public class ChessMatch {
     private Board board;
 
     public ChessMatch(){
         board = new Board(8,8);
+        initialSetup();
     }
 
     public ChessPiece[][] getPieces(){
@@ -17,5 +20,8 @@ public class ChessMatch {
             }
         }
         return mat;
+    }
+    private void initialSetup(){
+        board.placePiece(new Rook(board,Color.WHITE),new Position(2,1));
     }
 }
